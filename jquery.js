c7eff0d89821dbafc094edc0efd62cc163f28b79
ric2b <ricardo.amendoeira@ist.utl.pt>
2015-07-15 23:42:59 +0100

    trying to get the favicon to change dynamically, about to remove some lines

--- jquery.js
+++ jquery.js
@@ -1,5 +1,17 @@
-
 const POLL_PERIOD = 5000; // in milliseconds
+
+function changeFavicon(src {
+	var link = document.createElement('link'),
+		oldLink = document.getElementById('dynamic-favicon');
+	link.id = 'dynamic-favicon';
+	link.rel = 'icon';
+	link.href = src;
+	toggled = !toggled;
+	if (oldLink) {
+	 document.head.removeChild(oldLink);
+	}
+	document.head.appendChild(link);
+}
 
 $( document ).ready(function() {
 
@@ -10,11 +22,20 @@ $( document ).ready(function() {
 		$.get("occupation", function(data){
 			if(data == "free") {
 				$("#status-light").css("background-color", "limegreen");
+				function() {
+		    		changeFavicon('/favgreen.png');
+		   		};
 			} else if(data == "busy") {
 				$("#status-light").css("background-color", "red");
+				function() {
+		    		changeFavicon('/favred.png');
+		   		};
 			}
 		}).fail(function() {
 			$("#status-light").css("background-color", "lightgray");
+			function() {
+		    		changeFavicon('/favblack.png');
+		   		};
 		});
 	}
 });
