a0005032ef051d02cf7efa496e11348445e745cb
ric2b <ricardo.amendoeira@ist.utl.pt>
2015-07-20 16:33:36 +0100

    interface changes

--- checkOccupation.js
+++ checkOccupation.js
@@ -8,15 +8,18 @@ $( document ).ready(function() {
 	function checkOccupation() {
 		$.get("occupation", function(data){
 			if(data == "free") {
-				$("#status-light").css("background-color", "limegreen");
+			//	$("#status-light").css("background-color", "limegreen");
+				document.body.style.background="green";
 				favicon.change('/favgreen.png');
 		   		
 			} else if(data == "busy") {
-				$("#status-light").css("background-color", "red");
+			//	$("#status-light").css("background-color", "red");
+				document.body.style.background="red";
 				favicon.change('/favred.png');
 			}
 		}).fail(function() {
-			$("#status-light").css("background-color", "lightgray");
+			//$("#status-light").css("background-color", "lightgray");
+			document.body.style.background="lightgray";
 			favicon.change('/favblack.png');
 		});
 	}
